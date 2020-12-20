a4396919848a41447672bdc1a1d72d585c7950e7
nguyenhuyenag <nguyenhuyenag@users.noreply.github.com>
2020-12-20 14:05:46 +0700

    #

--- mongodb/src/main/java/com/App.java
+++ mongodb/src/main/java/com/App.java
@@ -1,14 +1,9 @@
 package com;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class App {
-	
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		List<String> list = new ArrayList<String>();
-		list.forEach(t -> System.out.println(t));
+		System.out.println(System.getProperty("mongodb.uri"));
 	}
-	
+
 }
---
+++ mongodb/src/main/java/com/MyConstants.java
@@ -0,0 +1,9 @@
+package com;
+
+public class MyConstants {
+
+	// Có thể Database này không tồn tại trên MongoDB của bạn
+	// Nhưng nó sẽ được tự động tạo ra, không cần phải thay đổi trường này
+	public static final String DB_NAME = "mongodb://localhost:27017";
+
+}
