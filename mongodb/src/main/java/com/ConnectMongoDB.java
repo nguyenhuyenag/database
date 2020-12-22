04782e9a73ad1b755064294519ad8f25233d6472
nguyenhuyenag <nguyenhuyenag@users.noreply.github.com>
2020-12-22 20:11:34 +0700

    #

--- mongodb/src/main/java/com/ConnectMongoDB.java
+++ mongodb/src/main/java/com/ConnectMongoDB.java
@@ -40,69 +40,70 @@ public class ConnectMongoDB {
 
 	public static void main(String[] args) {
 		Logger.getLogger("org.mongodb.driver").setLevel(Level.WARNING);
-		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-		MongoDatabase database = mongoClient.getDatabase("mydb");
-		MongoCollection<Document> collection = database.getCollection("test");
+		try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");) {
+			MongoDatabase database = mongoClient.getDatabase("mydb");
+			MongoCollection<Document> collection = database.getCollection("test");
 
-		// Insert one
-		Document doc = new Document("name", "MongoDB");
-		doc.append("type", "database");
-		doc.append("count", 1);
-		doc.append("versions", Arrays.asList("v3.2", "v3.0", "v2.6"));
-		doc.append("info", new Document("x", 203).append("y", 102));
-		// collection.insertOne(doc);
+			// Insert one
+			Document doc = new Document("name", "MongoDB");
+			doc.append("type", "database");
+			doc.append("count", 1);
+			doc.append("versions", Arrays.asList("v3.2", "v3.0", "v2.6"));
+			doc.append("info", new Document("x", 203).append("y", 102));
+			// collection.insertOne(doc);
 
-		// Insert many
-		List<Document> list = new ArrayList<>();
-		for (int i = 6; i < 20; i++) {
-			list.add(new Document("i", i));
-		}
-		// collection.insertMany(list);
+			// Insert many
+			List<Document> list = new ArrayList<>();
+			for (int i = 6; i < 20; i++) {
+				list.add(new Document("i", i));
+			}
+			// collection.insertMany(list);
 
-		// Count documents in a collection
-		// System.out.println(collection.countDocuments());
+			// Count documents in a collection
+			// System.out.println(collection.countDocuments());
 
-		// Find the first document
-		Document myDoc = collection.find().first();
-		// System.out.println(myDoc.toJson(MyConstants.prettyPrint));
-		
-		// Find all document
-		FindIterable<Document> cursor = collection.find();
-		for (Document d : cursor) {
-			// System.out.println(d.toJson());
+			// Find the first document
+			Document myDoc = collection.find().first();
+			// System.out.println(myDoc.toJson(MyConstants.prettyPrint));
+
+			// Find all document
+			FindIterable<Document> cursor = collection.find();
+			for (Document d : cursor) {
+				// System.out.println(d.toJson());
+			}
+
+			// Find equal (=)
+			myDoc = collection.find(eq("i", 4)).first(); // =
+			// System.out.println(myDoc.toJson());
+
+			// Find greater (>)
+			cursor = collection.find(gt("i", 10));
+			for (Document d : cursor) {
+				// System.out.println(d.toJson());
+			}
+
+			// Find less than or equal (<=)
+			collection.find(and(gt("i", 10), lte("i", 15)));
+			for (Document d : cursor) {
+				// System.out.println(d.toJson());
+			}
+
+			// Update a single docmuent
+			UpdateResult update = collection.updateOne(eq("i", 10), new Document("$set", new Document("i", 110)));
+			System.out.println(update);
+
+			// Delete a single document
+			DeleteResult deleteResult = collection.deleteOne(eq("i", 0));
+			// System.out.println(deleteResult.getDeletedCount());
+
+			Bson filter = gte("i", 10);
+			DeleteResult deleteAllResult = collection.deleteMany(filter);
+			System.out.println(deleteAllResult.getDeletedCount());
+
+			// Create indexes
+			// 1 là tăng dần, -1 là giảm dần
+			collection.createIndex(new Document("i", 1));
 		}
-				
-		// Find equal (=)
-		myDoc = collection.find(eq("i", 4)).first(); // =
-		// System.out.println(myDoc.toJson());
-		
-		// Find greater (>)
-		cursor = collection.find(gt("i", 10));
-		for (Document d : cursor) {
-			// System.out.println(d.toJson());
-		}
-		
-		// Find less than or equal (<=)
-		collection.find(and(gt("i", 10), lte("i", 15)));
-		for (Document d : cursor) {
-			// System.out.println(d.toJson());
-		}
-		
-		// Update a single docmuent
-		UpdateResult update = collection.updateOne(eq("i", 10), new Document("$set", new Document("i", 110)));
-		System.out.println(update);
-		
-		// Delete a single document
-		DeleteResult deleteResult = collection.deleteOne(eq("i", 0));
-		// System.out.println(deleteResult.getDeletedCount());
-		
-		Bson filter = gte("i", 10);
-		DeleteResult deleteAllResult = collection.deleteMany(filter);
-		System.out.println(deleteAllResult.getDeletedCount());
-		
-		// Create indexes
-		// 1 là tăng dần, -1 là giảm dần
-		collection.createIndex(new Document("i", 1));
 	}
 
 }
