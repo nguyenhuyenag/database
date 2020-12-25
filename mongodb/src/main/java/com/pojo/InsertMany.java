e4ebd894ea029f3cc0ccab3dbc9bb4938210286e
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2020-12-25 10:00:11 +0700

    #

--- mongodb/src/main/java/com/pojo/InsertMany.java
+++ mongodb/src/main/java/com/pojo/InsertMany.java
@@ -7,6 +7,7 @@ import java.util.logging.Logger;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
@@ -16,31 +17,54 @@ import com.mongodb.client.model.InsertManyOptions;
 
 public class InsertMany {
 
-	// private static final Random rand = new Random();
+	static String id = "5fe54d726c6be435045b659";
 
+	public static void insertOne(MongoCollection<Document> collection) {
+		Document doc = new Document("_id", new ObjectId(id + 5));
+		doc.append("text", RandomStringUtils.randomAlphabetic(5).toUpperCase());
+		collection.insertOne(doc);
+	}
+
+	public static void insertMany(MongoCollection<Document> collection) {
+		List<Document> list = new ArrayList<>();
+		for (int i = 1; i < 10; i++) {
+			Document doc = new Document("_id", new ObjectId(id + i));
+			doc.append("text", RandomStringUtils.randomAlphabetic(5).toUpperCase());
+			list.add(doc);
+		}
+		collection.insertMany(list);
+	}
+
+	public static void insertManyOptions(MongoCollection<Document> collection) {
+		List<Document> list = new ArrayList<>();
+		for (int i = 1; i < 10; i++) {
+			Document doc = new Document("_id", new ObjectId(id + i));
+			doc.append("text", RandomStringUtils.randomAlphabetic(5).toUpperCase());
+			list.add(doc);
+		}
+		collection.insertMany(list, new InsertManyOptions().ordered(false));
+	}
+
+	/**
+	 * Mặc định MongoDB sẽ insert danh sách theo thứ tự, nếu có 1 Document bị lỗi
+	 * (ví dụ DuplicateKeyException) thì phần còn lại của danh sách sẽ không được
+	 * insert. Thuộc tính `new InsertManyOptions().ordered(false)` cho phép tiếp tục
+	 * insert các Document (không có lỗi) còn lại.
+	 */
 	public static void main(String[] args) {
 		Logger.getLogger("org.mongodb.driver").setLevel(Level.WARNING);
 		try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
 			MongoDatabase sampleTrainingDB = mongoClient.getDatabase("sample_training");
-			MongoCollection<Document> collection = sampleTrainingDB.getCollection("test_insert_many");
-			
-			// Document doc = new Document("_id", new ObjectId());
-			// Document doc = new Document();
-			// doc.append("text", RandomStringUtils.randomAlphabetic(5).toUpperCase());
-			// collection.insertOne(doc);
-			
-			List<Document> list = new ArrayList<>();
-			for (int i = 1; i < 10; i++) {
-				Document doc = new Document();
-				doc.append("text", RandomStringUtils.randomAlphabetic(5).toUpperCase());
-				list.add(doc);
-			}
-			String _id = collection.find().first().get("_id").toString();
-			Document first = new Document("_id", _id).append("text", RandomStringUtils.randomAlphabetic(5).toUpperCase());
-			list.add(first);
-			collection.insertMany(list);
-			// collection.insertMany(list, new InsertManyOptions().ordered(true));
-			
+			MongoCollection<Document> collection = sampleTrainingDB.getCollection("insert_many");
+
+			// insertOne(collection);
+
+			// insertMany(collection);
+
+			insertManyOptions(collection);
+
+			// collection.drop();
+
 			System.out.println("OK!");
 		}
 
